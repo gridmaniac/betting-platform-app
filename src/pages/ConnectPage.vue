84ea4b196d80542ae18e4fdf6df10f74c1063991
Dmitry Kungurov <yepsconnect@icloud.com>
2022-08-30 18:18:11 +0400

    fix

--- src/pages/ConnectPage.vue
+++ src/pages/ConnectPage.vue
@@ -2,19 +2,19 @@
 import { useRoute, useRouter } from "vue-router";
 import { useAuthStore } from "@/stores/authStore";
 import { useWalletStore } from "@/stores/walletStore";
-const authStore = useAuthStore()
-const walletStore = useWalletStore()
+const authStore = useAuthStore();
+const walletStore = useWalletStore();
 
 const { params } = useRoute();
 const router = useRouter();
 const payload = atob(params.payload as string);
 const userData = JSON.parse(payload);
-authStore.token = userData.token
-localStorage.setItem('token', authStore.token)
-authStore.isAuth = true
-walletStore.currentAsset = userData.code
-router.push({ name: 'wallet'})
-walletStore.connectWallet()
+authStore.token = userData.token;
+localStorage.setItem("token", userData.token);
+authStore.isAuth = true;
+walletStore.currentAsset = userData.code;
+router.push({ name: "wallet" });
+walletStore.connectWallet();
 </script>
 
 <template>
