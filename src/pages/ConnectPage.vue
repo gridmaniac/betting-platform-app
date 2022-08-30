7301c1f035fbcfce1b9ba7b353318c006e81b59b
Dmitry Kungurov <yepsconnect@icloud.com>
2022-08-30 18:12:59 +0400

    add metamask to mobile

--- src/pages/ConnectPage.vue
+++ src/pages/ConnectPage.vue
@@ -1,4 +1,21 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useRoute, useRouter } from "vue-router";
+import { useAuthStore } from "@/stores/authStore";
+import { useWalletStore } from "@/stores/walletStore";
+const authStore = useAuthStore()
+const walletStore = useWalletStore()
+
+const { params } = useRoute();
+const router = useRouter();
+const payload = atob(params.payload as string);
+const userData = JSON.parse(payload);
+authStore.token = userData.token
+localStorage.setItem('token', authStore.token)
+authStore.isAuth = true
+walletStore.currentAsset = userData.code
+router.push({ name: 'wallet'})
+walletStore.connectWallet()
+</script>
 
 <template>
   <h1 class="text-3xl">BOBKA</h1>
