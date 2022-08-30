9de6c546117d4c546ade34ebd3d2a3f16778f547
Dmitry Kungurov <yepsconnect@icloud.com>
2022-08-30 18:41:35 +0400

    fix

--- src/pages/ConnectPage.vue
+++ src/pages/ConnectPage.vue
@@ -5,21 +5,24 @@ import { useWalletStore } from "@/stores/walletStore";
 const authStore = useAuthStore();
 const walletStore = useWalletStore();
 
-const { params } = useRoute();
-const router = useRouter();
-const payload = atob(params.payload as string);
-const userData = JSON.parse(payload);
-authStore.token = userData.token;
-localStorage.setItem("token", userData.token);
-authStore.isAuth = true;
-walletStore.currentAsset = userData.code;
-await walletStore.getWallet(true)
-if (userData.deposit) {
-  walletStore.deposit(userData.deposit)
-} else {
-  walletStore.connectWallet();
+async function connect() {
+  const { params } = useRoute();
+  const router = useRouter();
+  const payload = atob(params.payload as string);
+  const userData = JSON.parse(payload);
+  authStore.token = userData.token;
+  localStorage.setItem("token", userData.token);
+  authStore.isAuth = true;
+  walletStore.currentAsset = userData.code;
+  await walletStore.getWallet(true);
+  if (userData.deposit) {
+    walletStore.deposit(userData.deposit);
+  } else {
+    walletStore.connectWallet();
+  }
+  router.push({ name: "wallet" });
 }
-router.push({ name: "wallet" });
+connect()
 </script>
 
 <template>
